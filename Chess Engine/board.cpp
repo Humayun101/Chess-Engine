692d31f2ff2f5788b67e6e46cd2a29136283ff7a
Hunzala Humayun <120135038+Humayun101@users.noreply.github.com>
2024-04-11 21:50:14 +0500

    Add files via upload

--- Chess Engine/board.cpp
+++ Chess Engine/board.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
+int ranks;
+int files;
+for (files=0; files<8; files++){
+    for (ranks=0; ranks<8; ranks++){
+        cout<<"*";
 
-cout<<"Chess engine";
-return 0;
-    
+
+
+    }
+cout<<endl;
+}
+    return 0;
 }
