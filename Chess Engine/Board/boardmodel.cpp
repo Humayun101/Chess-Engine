fb6ebdda778fa2c2b7f5d19ead25107e3ada611b
Hunzala Humayun <120135038+Humayun101@users.noreply.github.com>
2024-04-15 20:36:56 +0500

    Add files via upload

---
+++ Chess Engine/Board/boardmodel.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int ranks;
+int files;
+
+string lightCol= "W";
+string darkCol= "B";
+
+
+for (files=0; files<8; files++){
+    for (ranks=0; ranks<8; ranks++){
+        bool lightbox=(files+ranks)%2 !=0;
+
+        auto boxcolor = (lightbox)? lightCol: darkCol;
+        cout<<boxcolor<<" ";
+        
+
+
+
+    }
+cout<<endl;
+}
+    return 0;
+}
